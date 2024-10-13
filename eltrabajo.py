f65fdf3b54f732db7d98fba97790d45dd89ca9d8
siz1209 <simonjonasvasquez@gmail.com>
2024-10-13 12:53:02 -0500

    Create eltrabajo.py

---
+++ eltrabajo.py
@@ -0,0 +1,12 @@
+diccionario={
+    "JUICIOSO":"persona responsable",
+    "CHIMBA":"algo que las espectativas o algo bonito",
+    "CRISPETAS":"también conocidas como , rosas, pochoclos, cabritas",
+    "AGUACATE":"también conocido como palta"
+}
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+#print(diccionario.keys())
+if word in diccionario.keys():
+    # ¿Qué debemos hacer si se encuentra la palabra?
